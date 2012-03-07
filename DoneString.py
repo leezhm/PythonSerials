928f592ed90e710e451dd7630c4a12841fd26773
Freedom Lee <leezhm@126.com>
2012-03-07 17:26:40 +0800

    Chapter 3 Data Type String

---
+++ DoneString.py
@@ -0,0 +1,23 @@
+'''
+	DoneString.py
+	
+	Written by Leezhm on 7th March, 2012
+	
+	Copyright (c) leezhm@126.com, All Right Reserved.
+	
+	For Chapter 3 String, and Your First Program 
+
+    <<Invent Your Own Computer Games with Python>>
+	
+'''
+
+spam = 'hellow'
+print(spam)
+
+#This Program says hello and asks for your name.
+print('Hello' + "World")
+print('What is your name ?')
+name = input();
+print('Nice to meet you, ' + name)
+
+
