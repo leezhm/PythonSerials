a08d7dd715c8e6d5159245a7b9834bb820020a38
Freedom Lee <leezhm@126.com>
2012-03-09 02:20:37 +0800

    Add Jokes.py for Chapter 5

---
+++ Jokes.py
@@ -0,0 +1,28 @@
+'''
+#	Jokes.py
+#	
+#	Written by Leezhm on 9th March, 2012.
+#	
+#	Copyright (c) leezhm@126.com, All Right Reserved.
+#	
+#	For Chapter 4 Jokesw
+#	
+#	<<Invent Your Own Computer Games with Python>>
+#	
+'''
+
+
+print('What do you get when you cross a snowman with a vampire?')
+input()
+print('Frostbite!')
+print()
+print('What do dentists call a astronaut\'s cavity?')
+input()
+print('A black hole!')
+print()
+print('Knock knock.')
+input()
+print("Interrupting cow.")
+input()
+print('Interrupting cow wh', end='')
+print('-MOO!')
