737899b4705f0bd9dde44f8d3e142c092730c4f6
Freedom Lee <leezhm@126.com>
2012-03-12 22:11:15 +0800

    Add DragonRealm to Chapter 6

---
+++ DragonRealm.py
@@ -0,0 +1,18 @@
+'''
+#	DragonRealm.py
+#	
+#	Written by leezhm on 10th March, 2012.
+#
+#	Copyright (c) leezhm@126.com, All Right Reserved.
+#
+#	For Chapter 4 Dragon Realm
+#
+#	<<Invent Your Own Computer Games with Python>>
+#
+'''
+
+print('For Chapter 4 Dragon Realm')
+
+str = 'Welcome to china!'
+print(len(str))
+
