170df4a85bd81b19e876c73753237248a8d6333b
Freedom Lee <leezhm@126.com>
2012-03-13 23:13:09 +0800

    Add VariableScope for Chapter 6. It's about Variable Scope

--- VariableScope.py
+++ VariableScope.py
@@ -1,1 +1,13 @@
-__author__ = 'Leezhm'
+'''
+#   VariableScope.py
+#
+#   Written by leezhm on 13th March, 2012.
+#
+#   Copyright (C) leezhm(c)126.com. All Right Reserved.
+#
+#   For Chapter 6 Dragon Realm
+#
+#   <<Invent Your Own Computer Games with Python>>
+'''
+
+import time
