2897735ce19704fb16b62e3bdc42e26a3d722060
Freedom Lee <leezhm@126.com>
2012-03-20 00:14:43 +0800

    Add UsingDebugger for Chapter 7 It is a empty file except Header.I have not install the IDLE

---
+++ UsingDebugger.py
@@ -0,0 +1,13 @@
+'''
+#   UsingDebugger.py
+#
+#   Written by leezhm(c)126.com, 19th March, 2012.
+#
+#   Copyright (c) leezhm(c)126.com. All Right Reserved.
+#
+#   Chapter 7 Using the Debugger
+#
+#   <<Invent Your Own Computer Games with Python>>
+'''
+
+
